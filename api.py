2541a3b6ad09dee8ef14ef4f2d3ac85d71d44bb2
Anh Khoa Phan Le <dolphy.phanle@gmail.com>
2023-08-20 08:15:15 +0100

    fix api error

--- api.py
+++ api.py
@@ -18,7 +18,27 @@ def get_klines():
     conn.close()
 
     # Convert data to a list of dictionaries for JSON serialization
-    klines = [{"id": kline[0], "symbol": kline[1], ...} for kline in data]  # expand for all columns
+    # klines = [{"id": kline[0], "symbol": kline[1], ...} for kline in data]  # expand for all columns
+    klines = [
+        {
+            "id": kline[0],
+            "symbol": kline[1],
+            "interval": kline[2],
+            "open_time": kline[3],
+            "open": kline[4],
+            "high": kline[5],
+            "low": kline[6],
+            "close": kline[7],
+            "volume": kline[8],
+            "close_time": kline[9],
+            "quote_asset_volume": kline[10],
+            "trades": kline[11],
+            "taker_buy_base_asset_volume": kline[12],
+            "taker_buy_quote_asset_volume": kline[13],
+            "ignore_column": kline[14]
+        }
+        for kline in data
+    ]
 
     return jsonify(klines)
 
@@ -36,7 +56,20 @@ def get_aggregated_trades():
     conn.close()
 
     # Convert data to a list of dictionaries for JSON serialization
-    aggregated_trades = [{"agg_trade_id": trade[0], "symbol": trade[1], ...} for trade in data]  # expand for all columns
+    # aggregated_trades = [{"agg_trade_id": trade[0], "symbol": trade[1], ...} for trade in data]  # expand for all columns
+    aggregated_trades = [
+        {
+            "agg_trade_id": trade[0],
+            "symbol": trade[1],
+            "price": trade[2],
+            "quantity": trade[3],
+            "first_trade_id": trade[4],
+            "last_trade_id": trade[5],
+            "transact_time": trade[6],
+            "is_buyer_maker": bool(trade[7])  # Convert integer to boolean for JSON
+        }
+        for trade in data
+    ]
 
     return jsonify(aggregated_trades)
 
@@ -66,8 +99,43 @@ def get_vp_data():
     conn.close()
 
     # Convert data to a list of dictionaries for JSON serialization
-    klines = [{"id": kline[0], "symbol": kline[1], ...} for kline in klines_data]  # expand for all columns
-    aggregated_trades = [{"agg_trade_id": trade[0], "symbol": trade[1], ...} for trade in aggregated_trades_data]  # expand for all columns
+    klines = [
+        {
+            "id": kline[0],
+            "symbol": kline[1],
+            "interval": kline[2],
+            "open_time": kline[3],
+            "open": kline[4],
+            "high": kline[5],
+            "low": kline[6],
+            "close": kline[7],
+            "volume": kline[8],
+            "close_time": kline[9],
+            "quote_asset_volume": kline[10],
+            "trades": kline[11],
+            "taker_buy_base_asset_volume": kline[12],
+            "taker_buy_quote_asset_volume": kline[13],
+            "ignore_column": kline[14]
+        }
+        for kline in data
+    ]
+
+
+
+    aggregated_trades = [
+        {
+            "agg_trade_id": trade[0],
+            "symbol": trade[1],
+            "price": trade[2],
+            "quantity": trade[3],
+            "first_trade_id": trade[4],
+            "last_trade_id": trade[5],
+            "transact_time": trade[6],
+            "is_buyer_maker": bool(trade[7])  # Convert integer to boolean for JSON
+        }
+        for trade in data
+    ]
+
 
     # Merge or join data. Here, we're just returning them as separate lists, but you can merge based on your criteria
     combined_data = {
